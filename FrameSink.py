9af3c15c94e21301cd65a5c6a89755313a3bd9e0
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2015-02-20 13:25:53 +0100

    Added pysdl2 framesink

--- FrameSink.py
+++ FrameSink.py
@@ -109,6 +109,42 @@ class SDLRenderer(FrameSink):
 		self.window = pygame.display.set_mode((frame.width, frame.height))
 		self.overlay = pygame.Overlay(pygame.YV12_OVERLAY, (frame.width, frame.height))
 
+import sdl2, ctypes
+
+class SDL2Renderer(FrameSink):
+
+	cmdLineKey = "sdl2"
+
+	def start(self):
+		sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
+		self.window = None
+
+	def handle(self, frame, timestamp):
+		if self.window is None:
+			self.setupWindow(frame.width, frame.height)
+
+		sdl2.SDL_UpdateTexture(self.texture, None, "%s%s%s" % (frame.y, frame.v, frame.u), frame.width) 
+		sdl2.render.SDL_RenderClear(self.renderer)
+		sdl2.render.SDL_RenderCopy(self.renderer, self.texture, None, None)
+		sdl2.render.SDL_RenderPresent(self.renderer)
+		sdl2.SDL_PollEvent(None)
+
+	def finish(self):
+		if self.window:
+			sdl2.SDL_DestroyTexture(self.texture)
+			sdl2.SDL_DestroyRenderer(self.renderer)
+			sdl2.SDL_DestroyWindow(self.window)
+		sdl2.SDL_Quit()
+
+	def setupWindow(self, width, height):
+		self.window = sdl2.SDL_CreateWindow("%s: %s" % (config.sdl_window_caption, self.clientName), sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED, width, height, sdl2.SDL_WINDOW_SHOWN)
+
+		self.renderer = sdl2.SDL_CreateRenderer(self.window, -1, sdl2.SDL_RENDERER_ACCELERATED)
+		self.texture = sdl2.SDL_CreateTexture(self.renderer, sdl2.SDL_PIXELFORMAT_YV12, sdl2.SDL_TEXTUREACCESS_STREAMING, width, height)
+		
+
+
+
 ##############################################################################
 
 availableSinks = {}
